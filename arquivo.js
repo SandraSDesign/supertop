d818f3554ba4d3ed93e58e08c99c6997365bd161
SandraSDesign <sandraschmitteldesign@gmail.com>
2024-04-10 14:46:26 -0300

    Super Top

---
+++ arquivo.js
@@ -0,0 +1,7 @@
+// Aqui você pode adicionar funcionalidades JavaScript conforme necessário<script>
+    function adicionarAoCarrinho() {
+        var quantidade = document.getElementById("quantidade").value;
+        // Aqui você pode fazer o que quiser com a quantidade selecionada, como enviar para um carrinho de compras
+        console.log("Quantidade selecionada: " + quantidade);
+    }
+
